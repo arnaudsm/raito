68d7de424930cff286abf7731caee2544344cf98
arnaudsm <arnaudsm66@gmail.com>
2023-07-15 02:27:04 +0200

    Clean server

--- dev/server.js
+++ dev/server.js
@@ -6,48 +6,24 @@ import fs from "fs";
 
 const root = path.join(path.dirname(fileURLToPath(import.meta.url)), "..");
 
-const hashRouterApp = () => {
-  const app = Fastify();
-  app.register(fastifyStatic, { root });
-  app.listen({ port: 3000 });
-  console.log("Hash Router: http://localhost:3000");
-}
-
-const browserRouterApp = () => {
+const app = ({ port, rewrite, prefix }) => {
   const app = Fastify();
   let html = fs.readFileSync("../index.html", "utf-8")
-    .replace("browserRouter: false,", "browserRouter: true,")
+  if (rewrite) html = rewrite(html);
 
   app.register(fastifyStatic, {
     root,
     wildcard: false,
+    prefix,
     allowedPath: (path) => !["index.html", "/"].includes(path),
   });
   app.setNotFoundHandler((_, reply) => {
     reply.code(200).type("text/html").send(html);
   });
-  app.listen({ port: 3001 });
-  console.log("Browser Router: http://localhost:3001");
+  app.listen({ port });
+  console.log(`Browser Router: http://localhost:${port}`);
 };
 
-const subdirApp = () => {
-  const app = Fastify();
-  let html = fs.readFileSync("../index.html", "utf-8")
-    .replace('sitePath: "/"', 'sitePath: "/subdir/"')
-
-  app.register(fastifyStatic, {
-    root,
-    wildcard: false,
-    prefix: "/subdir/",
-    allowedPath: (path) => !["/index.html", "/"].includes(path),
-  });
-  app.setNotFoundHandler((_, reply) => {
-    reply.code(200).type("text/html").send(html);
-  });
-  app.listen({ port: 3002 });
-  console.log("Subdir: http://localhost:3002");
-};
-
-hashRouterApp();
-browserRouterApp();
-subdirApp();
+app({ port: 3000 });
+app({ port: 3001, rewrite: (html) => html.replace("browserRouter: false,", "browserRouter: true,") });
+app({ port: 3002, rewrite: (html) => html.replace('sitePath: "/"', 'sitePath: "/subdir/"'), prefix: "/subdir/" });
