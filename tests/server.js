ac03c674ca1b4a9032fef9f4263fb8251099cc5c
arnaudsm <arnaud.desaintmeloir@gmail.com>
2023-11-25 17:54:08 +0100

    Rename Tests

--- tests/server.js
+++ tests/server.js
@@ -6,24 +6,24 @@ import fs from "fs";
 
 const root = path.join(path.dirname(fileURLToPath(import.meta.url)), "..");
 
-const app = ({ port, rewrite, prefix }) => {
-  const app = Fastify();
-  let html = fs.readFileSync("../index.html", "utf-8")
-  if (rewrite) html = rewrite(html);
+const app = ({ port, rewrite, prefix, name = "Default Routing" }) => {
+	const app = Fastify();
+	let html = fs.readFileSync("../index.html", "utf-8");
+	if (rewrite) html = rewrite(html);
 
-  app.register(fastifyStatic, {
-    root,
-    wildcard: false,
-    prefix,
-    allowedPath: (path) => !["index.html", "/"].includes(path),
-  });
-  app.setNotFoundHandler((_, reply) => {
-    reply.code(200).type("text/html").send(html);
-  });
-  app.listen({ port });
-  console.log(`Browser Router: http://localhost:${port}`);
+	app.register(fastifyStatic, {
+		root,
+		wildcard: false,
+		prefix,
+		allowedPath: (path) => !["index.html", "/"].includes(path),
+	});
+	app.setNotFoundHandler((_, reply) => {
+		reply.code(200).type("text/html").send(html);
+	});
+	app.listen({ port });
+	console.log(`${name}: http://localhost:${port}`);
 };
 
 app({ port: 3000 });
-app({ port: 3001, rewrite: (html) => html.replace("browserRouter: false,", "browserRouter: true,") });
-app({ port: 3002, rewrite: (html) => html.replace('sitePath: "/"', 'sitePath: "/subdir/"'), prefix: "/subdir/" });
+app({ port: 3001, name: "Browser Routing", rewrite: (html) => html.replace("browserRouter: false,", "browserRouter: true,") });
+app({ port: 3002, name: "Subdir Routing ", rewrite: (html) => html.replace('sitePath: "/"', 'sitePath: "/subdir/"'), prefix: "/subdir/" });
